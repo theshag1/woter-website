5ba248fae2f5ddcee2ab102643c87f75a8421d8d
islomov_ibrohimjon <isloomoov@gmail.com>
2023-04-27 12:07:11 +0500

    wote

--- woters/settings.py
+++ woters/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRETKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
